638cfa6ec59b8b790d2159d4a98a6d15448ce81f
Alex Tessaract <87167056+AlexTessaract@users.noreply.github.com>
2022-02-11 22:54:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,6 @@ def after():
     return render_template('after.html', data=final_prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
 
 
